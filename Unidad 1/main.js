5b0f7f1c6869bd748f3d6f224f7a17adbfd969df
Juan Lesieur <juanma.lesieur@gmail.com>
2022-03-29 22:09:53 -0300

    Unidad 1

---
+++ Unidad 1/main.js
@@ -0,0 +1,7 @@
+let dia = prompt('Ingrese su día de nacimiento(en numero)')
+let mes = prompt('Ingrese su mes de nacimiento(en numero)')
+let año = prompt('Ingrese su año de nacimiento')
+let numFav = prompt('Ingrese su numero favorito')
+
+console.log('Su fecha de nacimiento es:' + dia + '/' + mes + '/' + año);
+console.log('Su numero favorito es: ' + numFav);
